c517dc265db76481ec880a220f01c99d36614209
Darwinst98 <djsimba@espe.edu.ec>
2023-08-07 19:45:43 -0500

    Actualización Repositorio

--- App_Usuario/ios/Flutter/flutter_export_environment.sh
+++ App_Usuario/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\DELL\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\DELL\Documents\DARWIN\APP\ruebaflutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\DELL\Downloads\GestionAutobuses-RegistroHorarios\App_Usuario"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
